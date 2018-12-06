e66132db574f4b99375dd8f5e48815df2e864b28
lizascharf <lizascharf@gmail.com>
2018-12-06 14:22:47 -0500

    adding nyt

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ if (process.argv.length !== 4) {
   process.exit(1);
 }
 // Based on https://nodejs.org/docs/latest/api/process.html#process_process_argv
-const hostname = (process.argv.length === 3) ? process.argv[2] : '0.0.0.0';
-const port = process.env.PORT || 8080;
+// const hostname = (process.argv.length === 3) ? process.argv[2] : '0.0.0.0';
+// const port = process.env.PORT || 8080;
 //comment out the above two lines and uncomment the below two lines to run locally without heroku
-//const hostname = process.argv[2];
-//const port = process.argv[3];
+const hostname = process.argv[2];
+const port = process.argv[3];
 
 
 // Check valid inputs, using https://www.npmjs.com/package/validator
@@ -140,22 +140,45 @@ app.get('/house/:houseId', (req, res) => {
     // change to houseInfo
     const house = results.house_info;
     const houseName = house.name;
-    // Search Hollis for house
-    const url = `http://api.lib.harvard.edu/v2/items.json?title=${houseName}+house`;
-    fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        res.render('house', {
-          house: results.house_info,
-          title: houseName,
-          rooms: results.rooms_info,
-          events: results.events_info,
-          featuredRooms: results.featuredRooms_info,
-          resources: data.items.mods,
-          user: req.session.user,
-          month_name: month,
+    const eventResults = results.events_info;
+    for(const i in eventResults) {
+      // Default url would be 
+      eventResults[i].article = 'https://www.google.com/';
+      const url = new URL('https://api.nytimes.com/svc/search/v2/articlesearch.json');
+      const params = {
+        'api-key': 'e8ebee351d174f58bc3086a7917d1509',
+        q: `${eventResults[i].year}`,
+        begin_date: `${eventResults[i].year}` + '0101',
+        end_date: `${eventResults[i].year}` + '1231',
+      };
+      Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+      fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+          if(data.response) {
+            eventResults[i].article = data.response.docs[0].web_url;
+          }
         });
-      });
+      };
+    function waiting() {
+      // Search Hollis for house
+      const url = `http://api.lib.harvard.edu/v2/items.json?title=${houseName}+house`;
+      fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+          res.render('house', {
+            house: results.house_info,
+            title: houseName,
+            rooms: results.rooms_info,
+            events: eventResults,
+            featuredRooms: results.featuredRooms_info,
+            resources: data.items.mods,
+            user: req.session.user,
+            month_name: month,
+          });
+        });
+      }
+    setTimeout(waiting, 1500);
   });
 });
 
